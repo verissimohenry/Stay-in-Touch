004f97bcbb61fd01d121ba2e49c0592d2f67f326
verissimohenry <verissimohenry04@gmail.com>
2021-06-01 11:44:59 +0100

    add rspec file

---
+++ spec/models/friendss_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Friendss, type: :model do
+  context ' test friendss model' do
+    before do
+      @user = User.create(name: 'henry', email: 'verissimohenry04@gmail.com', password: '123456')
+      @friend = User.create(name: 'tobi', email: 'tobi@gmail.com', password: '123456')
+    end
+    it 'return false if friend_id is not provided' do
+      @invite = Friendss.create(user_id: @user.id)
+      expect(@invite).to be_valid
+    end
+    it 'return false if user_id is not provided' do
+      @invite = Friendss.create(friend_id: @friend.id)
+      expect(@invite).to be_valid
+    end
+
+    it 'return true if user_id and friend_id got provided' do
+      @invite = Friendss.create(user_id: @user.id, friend_id: @friend.id)
+      expect(@invite).to be_valid
+    end
+  end
+end
